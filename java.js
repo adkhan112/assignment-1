3d705a54313e2c3431ddc56f564a34827d093e24
muhammadfaizgit <mf691961@gmail.com>
2021-12-15 21:13:08 +0500

    commit

---
+++ java.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+//var firstname = prompt("Adeel");
+//var  lastname = prompt("khan");
+//var  fullname = prompt("Adeel Khan");
+ 
+var mymobname = "infinixhot8pro";
+console.log(mymobname.length);
+
+var  mycountryname = "pakistan";
+console.log( mycountryname.indexlength);
+
+
+
+
+
+
+ 
+
+
+ 
+
+ 
+
